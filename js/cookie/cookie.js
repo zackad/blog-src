e13924a600d2832501b983d8427722f488756662
zackad <zackad.dev@gmail.com>
2017-12-26 20:12:22 +0700

    cleanup debug message

--- js/cookie/cookie.js
+++ js/cookie/cookie.js
@@ -2,20 +2,10 @@
 
 import { cookie } from "cookie_js";
 
-// var cookie = require("cookie_js");
-
 var cookieDom = document.querySelector('.cookie-consent');
 var ccButton = document.querySelector('.cookie-consent .button');
 
-log('var');
-
-// log(cookie.get('cc'));
-
-// cookie.set('cc', 'agree');
-
 ccButton.addEventListener('click', acceptCookie);
-
-log('event');
 
 if (!cookie.get('cc')) {
     cookie.set('cc', 'deny', {expires: 30});
